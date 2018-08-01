403b2c0205f4364fa30f1a259f1d6fa8c0107994
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-08-01 08:35:24 +0200

    Autoclean VSE project

--- DependinatorVse/Commands/Private/DependinatorApiClient.cs
+++ DependinatorVse/Commands/Private/DependinatorApiClient.cs
@@ -9,141 +9,141 @@ using DependinatorApi.ApiHandling;
 
 namespace DependinatorVse.Commands.Private
 {
-	/// <summary>
-	/// Provides functions, which will forward the calls to the Dependinator application.
-	/// </summary>
-	public class DependinatorApiClient
-	{
-		private readonly string solutionFilePath;
-		private readonly bool isDeveloperStudio;
+    /// <summary>
+    ///     Provides functions, which will forward the calls to the Dependinator application.
+    /// </summary>
+    public class DependinatorApiClient
+    {
+        private readonly bool isDeveloperStudio;
+        private readonly string solutionFilePath;
 
 
-		public DependinatorApiClient(string solutionFilePath, bool isDeveloperStudio)
-		{
-			this.solutionFilePath = solutionFilePath;
-			this.isDeveloperStudio = isDeveloperStudio;
-		}
+        public DependinatorApiClient(string solutionFilePath, bool isDeveloperStudio)
+        {
+            this.solutionFilePath = solutionFilePath;
+            this.isDeveloperStudio = isDeveloperStudio;
+        }
 
 
-		public bool IsDependinatorInstalled => File.Exists(GetDependinatorPath());
+        public bool IsDependinatorInstalled => File.Exists(GetDependinatorPath());
 
 
-		/// <summary>
-		/// Show node, which corresponds to the specified file and line number.
-		/// </summary>
-		public async Task ShowFileAsync(string filePath, int lineNumber)
-		{
-			await CallAsync<IDependinatorApi>(api => api.ShowNodeForFile(filePath, lineNumber));
-		}
+        /// <summary>
+        ///     Show node, which corresponds to the specified file and line number.
+        /// </summary>
+        public async Task ShowFileAsync(string filePath, int lineNumber)
+        {
+            await CallAsync<IDependinatorApi>(api => api.ShowNodeForFile(filePath, lineNumber));
+        }
 
 
-		private async Task CallAsync<TRemoteService>(Action<TRemoteService> action)
-		{
-			string serverName = ApiServerNames.ServerName<TRemoteService>(solutionFilePath);
-			Log.Debug($"Calling: {serverName}");
+        private async Task CallAsync<TRemoteService>(Action<TRemoteService> action)
+        {
+            string serverName = ApiServerNames.ServerName<TRemoteService>(solutionFilePath);
+            Log.Debug($"Calling: {serverName}");
 
-			bool isStartedDependinator = false;
+            bool isStartedDependinator = false;
 
-			Stopwatch t = Stopwatch.StartNew();
+            Stopwatch t = Stopwatch.StartNew();
 
-			while (t.Elapsed < TimeSpan.FromSeconds(20))
-			{
-				try
-				{
-					if (ApiIpcClient.IsServerRegistered(serverName))
-					{
-						using (ApiIpcClient apiIpcClient = new ApiIpcClient(serverName))
-						{
-							action(apiIpcClient.Service<TRemoteService>());
-						}
+            while (t.Elapsed < TimeSpan.FromSeconds(20))
+            {
+                try
+                {
+                    if (ApiIpcClient.IsServerRegistered(serverName))
+                    {
+                        using (ApiIpcClient apiIpcClient = new ApiIpcClient(serverName))
+                        {
+                            action(apiIpcClient.Service<TRemoteService>());
+                        }
 
-						return;
-					}
+                        return;
+                    }
 
-					// DependinatorApi not yet registered, lets try to start Dependinator, or wait a little.
-					if (!isStartedDependinator)
-					{
-						isStartedDependinator = true;
-						StartDependinator(solutionFilePath);
-						await Task.Delay(300);
-					}
-					else
-					{
-						await Task.Delay(100);
-					}
-				}
-				catch (RemotingException e)
-				{
-					Log.Warn($"Call error, {e.Message}");
-				}
-				catch (Exception e)
-				{
-					Log.Error($"Failed to check if Dependiator instance is running, {e}");
-					throw;
-				}
-			}
+                    // DependinatorApi not yet registered, lets try to start Dependinator, or wait a little.
+                    if (!isStartedDependinator)
+                    {
+                        isStartedDependinator = true;
+                        StartDependinator(solutionFilePath);
+                        await Task.Delay(300);
+                    }
+                    else
+                    {
+                        await Task.Delay(100);
+                    }
+                }
+                catch (RemotingException e)
+                {
+                    Log.Warn($"Call error, {e.Message}");
+                }
+                catch (Exception e)
+                {
+                    Log.Error($"Failed to check if Dependiator instance is running, {e}");
+                    throw;
+                }
+            }
 
-			Log.Error("Failed to wait for other Dependiator instance");
-			throw new Exception("Timeout while waiting for Dependiator to start.");
-		}
-
-
-		private void StartDependinator(string filePath)
-		{
-			string targetPath = GetDependinatorPath();
-
-			Log.Debug($"Starting: {targetPath}");
-
-			StartProcess(targetPath, Quote(filePath));
-		}
+            Log.Error("Failed to wait for other Dependiator instance");
+            throw new Exception("Timeout while waiting for Dependiator to start.");
+        }
 
 
-		private static bool StartProcess(string targetPath, string arguments)
-		{
-			try
-			{
-				Process process = new Process();
-				process.StartInfo.FileName = Quote(targetPath);
-				process.StartInfo.Arguments = arguments;
+        private void StartDependinator(string filePath)
+        {
+            string targetPath = GetDependinatorPath();
 
-				process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-				process.StartInfo.CreateNoWindow = true;
-				process.StartInfo.UseShellExecute = false;
+            Log.Debug($"Starting: {targetPath}");
 
-				process.Start();
-				return true;
-			}
-			catch (Exception e)
-			{
-				Log.Error($"Failed to start {targetPath} {arguments}, {e}");
-				throw;
-			}
-		}
+            StartProcess(targetPath, Quote(filePath));
+        }
 
 
-		private static string Quote(string text)
-		{
-			char[] QuoteChar = "\"".ToCharArray();
-			text = text.Trim();
-			text = text.Trim(QuoteChar);
-			return $"\"{text}\"";
-		}
+        private static bool StartProcess(string targetPath, string arguments)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = Quote(targetPath);
+                process.StartInfo.Arguments = arguments;
+
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+
+                process.Start();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Failed to start {targetPath} {arguments}, {e}");
+                throw;
+            }
+        }
 
 
-		private string GetDependinatorPath() => Path.Combine(GetInstallFolderPath(), "Dependinator.exe");
+        private static string Quote(string text)
+        {
+            char[] QuoteChar = "\"".ToCharArray();
+            text = text.Trim();
+            text = text.Trim(QuoteChar);
+            return $"\"{text}\"";
+        }
 
 
-		private string GetInstallFolderPath()
-		{
-			if (isDeveloperStudio)
-			{
-				// While developing
-				return @"C:\Work Files\Dependinator\Dependinator\bin\Debug";
-			}
+        private string GetDependinatorPath() => Path.Combine(GetInstallFolderPath(), "Dependinator.exe");
 
-			string programFolderPath = Environment.ExpandEnvironmentVariables("%ProgramW6432%");
 
-			return Path.Combine(programFolderPath, "Dependinator");
-		}
-	}
+        private string GetInstallFolderPath()
+        {
+            if (isDeveloperStudio)
+            {
+                // While developing
+                return @"C:\Work Files\Dependinator\Dependinator\bin\Debug";
+            }
+
+            string programFolderPath = Environment.ExpandEnvironmentVariables("%ProgramW6432%");
+
+            return Path.Combine(programFolderPath, "Dependinator");
+        }
+    }
 }
--- DependinatorVse/VSPackage.cs
+++ DependinatorVse/VSPackage.cs
@@ -8,80 +8,79 @@ using DependinatorVse.Commands.Private;
 using DependinatorVse.Utils;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using Task = System.Threading.Tasks.Task;
+
 
 namespace DependinatorVse
 {
-	/// <summary>
-	/// This is the class that implements the package exposed by this assembly.
-	/// </summary>
-	/// <remarks>
-	/// <para>
-	/// The minimum requirement for a class to be considered a valid package for Visual Studio
-	/// is to implement the IVsPackage interface and register itself with the shell.
-	/// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-	/// to do it: it derives from the Package class that provides the implementation of the
-	/// IVsPackage interface and uses the registration attributes defined in the framework to
-	/// register itself and its components with the shell. These attributes tell the pkgdef creation
-	/// utility what data to put into .pkgdef file.
-	/// </para>
-	/// <para>
-	/// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-	/// </para>
-	/// </remarks>
-	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-	[InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)] // Info on this package for Help/About
-	[ProvideMenuResource("Menus.ctmenu", 1)]
-	[Guid(PackageGuids.guidShowInDependinatorCommandPackageString)]
-	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-	[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string, PackageAutoLoadFlags.BackgroundLoad)]
-	public sealed class VSPackage : AsyncPackage
-	{
-		private ApiManagerService apiManagerService;
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ShowInDependinatorCommand"/> class.
-		/// </summary>
-		public VSPackage()
-		{
-			// Inside this method you can place any initialization code that does not require
-			// any Visual Studio service because at this point the package object is created but
-			// not sited yet inside Visual Studio environment. The place to do all the other
-			// initialization is the Initialize method.
-		}
+    /// <summary>
+    ///     This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         The minimum requirement for a class to be considered a valid package for Visual Studio
+    ///         is to implement the IVsPackage interface and register itself with the shell.
+    ///         This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    ///         to do it: it derives from the Package class that provides the implementation of the
+    ///         IVsPackage interface and uses the registration attributes defined in the framework to
+    ///         register itself and its components with the shell. These attributes tell the pkgdef creation
+    ///         utility what data to put into .pkgdef file.
+    ///     </para>
+    ///     <para>
+    ///         To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...
+    ///         &gt; in .vsixmanifest file.
+    ///     </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID =
+        400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidShowInDependinatorCommandPackageString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly",
+        Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class VSPackage : AsyncPackage
+    {
+        private ApiManagerService apiManagerService;
 
 
-		/// <summary>
-		/// Initialization of the package; this method is called right after the package is sited, so this is the place
-		/// where you can put all the initialization code that rely on services provided by VisualStudio.
-		/// </summary>
-		/// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-		/// <param name="progress">A provider for progress updates.</param>
-		/// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-		{
-			try
-			{
-				// When initialized asynchronously, the current thread may be a background thread at this point.
-				// Do any initialization that requires the UI thread after switching to the UI thread.
-				await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        /// <summary>
+        ///     Initialization of the package; this method is called right after the package is sited, so this is the place
+        ///     where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">
+        ///     A cancellation token to monitor for initialization cancellation, which can occur when
+        ///     VS is shutting down.
+        /// </param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>
+        ///     A task representing the async work of package initialization, or an already completed task if there is none.
+        ///     Do not return null from this method.
+        /// </returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken,
+            IProgress<ServiceProgressData> progress)
+        {
+            try
+            {
+                // When initialized asynchronously, the current thread may be a background thread at this point.
+                // Do any initialization that requires the UI thread after switching to the UI thread.
+                await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
-				await ShowInDependinatorCommand.InitializeAsync(this);
+                await ShowInDependinatorCommand.InitializeAsync(this);
 
-				apiManagerService = new ApiManagerService();
-				await apiManagerService.InitApiServerAsync(this);
-			}
-			catch (Exception e)
-			{
-				Studio.ShowWarnMessageBox(this,
-					"Really sorry, the Dependinator Extension failed to initialize.\n" +
-					$"Error: {e.Message}\n" +
-					"Please report the issue at:\n" +
-					"https://github.com/michael-reichenauer/Dependinator/issues");
+                apiManagerService = new ApiManagerService();
+                await apiManagerService.InitApiServerAsync(this);
+            }
+            catch (Exception e)
+            {
+                Studio.ShowWarnMessageBox(this,
+                    "Really sorry, the Dependinator Extension failed to initialize.\n" +
+                    $"Error: {e.Message}\n" +
+                    "Please report the issue at:\n" +
+                    "https://github.com/michael-reichenauer/Dependinator/issues");
 
-				Log.Error($"Failed: {e}");
-			}
-		}
-	}
+                Log.Error($"Failed: {e}");
+            }
+        }
+    }
 }
