c116eae79d24b93447da184fdfd2c2fb5244a12a
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-08-01 08:36:40 +0200

    Autoclean update project

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -9,153 +9,153 @@ using System.Threading;
 
 namespace Updater
 {
-	class Program
-	{
-		private static readonly string ValidCertificateHash = "1CCB2B742FF45D6A3A4E42D34FD8497CE5212EF0";
+    internal class Program
+    {
+        private static readonly string ValidCertificateHash = "1CCB2B742FF45D6A3A4E42D34FD8497CE5212EF0";
 
-		private static readonly string ProgramName = "Dependinator";
-		private static readonly string SetupExeName = $"{ProgramName}Setup.exe";
-		private static readonly string RenewTaskName = $"{ProgramName} Renew";
-		private static readonly string UpdateTaskName = $"{ProgramName} Update";
-
-
-		static void Main(string[] args)
-		{
-			if (args.Contains("/register"))
-			{
-				Register();
-			}
-			else if (args.Contains("/update"))
-			{
-				TryUpdate();
-			}
-			else if (args.Contains("/renew"))
-			{
-				TryRenew();
-			}
-		}
+        private static readonly string ProgramName = "Dependinator";
+        private static readonly string SetupExeName = $"{ProgramName}Setup.exe";
+        private static readonly string RenewTaskName = $"{ProgramName} Renew";
+        private static readonly string UpdateTaskName = $"{ProgramName} Update";
 
 
-		/// <summary>
-		/// Registers the update as 2 scheduled windows tasks.
-		/// </summary>
-		private static void Register()
-		{
-			// Register TryUpdate task running as normal user, which will
-			// check for new updates and download new installer if available
-			string updateConfigPath = GetTaskConfigPath("Updater", 1, 10);
-			string updateArgs = $@"/Create /tn ""{UpdateTaskName}"" /F /XML ""{updateConfigPath}""";
-			Process.Start("schtasks", updateArgs)?.WaitForExit(5000);
-
-			// Register TryRenew task, which will run downloaded installer as SYSTEM user (admin rights)
-			string renewConfigPath = GetTaskConfigPath("Renewer", 1, 20);
-			string renewArgs = $@"/Create /tn ""{RenewTaskName}"" /F /XML ""{renewConfigPath}""";
-			Process.Start("schtasks", renewArgs)?.WaitForExit(5000);
-		}
-
-
-		private static void TryUpdate()
-		{
-			// Get path to Dependinator.exe
-			string programFolder = Path.GetDirectoryName(typeof(Program).Assembly.Location);
-			string dependinatorPath = Path.Combine(programFolder, $"{ProgramName}.exe");
-
-			// Run Dependinator.exe to check for and download new new installer filer
-			// (running as normal user without admin rights)
-			if (File.Exists(dependinatorPath))
-			{
-				Process.Start(dependinatorPath, "/checkupdate")?.WaitForExit(10000 * 60);
-			}
-		}
+        private static void Main(string[] args)
+        {
+            if (args.Contains("/register"))
+            {
+                Register();
+            }
+            else if (args.Contains("/update"))
+            {
+                TryUpdate();
+            }
+            else if (args.Contains("/renew"))
+            {
+                TryRenew();
+            }
+        }
 
 
-		private static void TryRenew()
-		{
-			// Get path to installer in ProgramData folder
-			string programData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-			string setupPath = Path.Combine(programData, ProgramName, SetupExeName);
+        /// <summary>
+        ///     Registers the update as 2 scheduled windows tasks.
+        /// </summary>
+        private static void Register()
+        {
+            // Register TryUpdate task running as normal user, which will
+            // check for new updates and download new installer if available
+            string updateConfigPath = GetTaskConfigPath("Updater", 1, 10);
+            string updateArgs = $@"/Create /tn ""{UpdateTaskName}"" /F /XML ""{updateConfigPath}""";
+            Process.Start("schtasks", updateArgs)?.WaitForExit(5000);
 
-			if (File.Exists(setupPath) && IsSignatureValid(setupPath))
-			{
-				// Run installer running as SYSTEM with admin rights
-				Process.Start(setupPath, "/VERYSILENT")?.WaitForExit(10000 * 60);
-			}
-
-			MarkFileAsDone(setupPath);
-		}
-
-
-		private static bool IsSignatureValid(string path)
-		{
-			try
-			{
-				X509Certificate certificate = X509Certificate.CreateFromSignedFile(path);
-				X509Certificate2 fileCertificate = new X509Certificate2(certificate);
-				return ValidCertificateHash == fileCertificate?.Thumbprint;
-			}
-			catch (Exception)
-			{
-				return false;
-			}
-		}
+            // Register TryRenew task, which will run downloaded installer as SYSTEM user (admin rights)
+            string renewConfigPath = GetTaskConfigPath("Renewer", 1, 20);
+            string renewArgs = $@"/Create /tn ""{RenewTaskName}"" /F /XML ""{renewConfigPath}""";
+            Process.Start("schtasks", renewArgs)?.WaitForExit(5000);
+        }
 
 
-		private static string GetTaskConfigPath(string configName, int startHour, int startMinute)
-		{
-			DateTime now = DateTime.Now;
-			string location = typeof(Program).Assembly.Location;
+        private static void TryUpdate()
+        {
+            // Get path to Dependinator.exe
+            string programFolder = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            string dependinatorPath = Path.Combine(programFolder, $"{ProgramName}.exe");
 
-			string date = now.ToString("s");
-			string startBoundary = new DateTime(now.Year, now.Month, now.Day, startHour, startMinute, 0)
-				.ToString("s");
-			string command = location;
-
-			string templateText = GetTaskConfigTemplate(configName);
-			string text = templateText
-				.Replace("{$Date}", date)
-				.Replace("{$StartBoundary}", startBoundary)
-				.Replace("{$Command}", command);
-
-			string filePath = $"{location}.{configName}.xml";
-			File.WriteAllText(filePath, text);
-
-			return filePath;
-		}
+            // Run Dependinator.exe to check for and download new new installer filer
+            // (running as normal user without admin rights)
+            if (File.Exists(dependinatorPath))
+            {
+                Process.Start(dependinatorPath, "/checkupdate")?.WaitForExit(10000 * 60);
+            }
+        }
 
 
-		private static string GetTaskConfigTemplate(string configName)
-		{
-			Assembly programAssembly = typeof(Program).Assembly;
-			string name = programAssembly.FullName.Split(',')[0];
-			string resourceName = $"{name}.{configName}.xml";
+        private static void TryRenew()
+        {
+            // Get path to installer in ProgramData folder
+            string programData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string setupPath = Path.Combine(programData, ProgramName, SetupExeName);
 
-			using (Stream stream = programAssembly.GetManifestResourceStream(resourceName))
-			using (StreamReader reader = new StreamReader(stream))
-			{
-				return reader.ReadToEnd();
-			}
-		}
+            if (File.Exists(setupPath) && IsSignatureValid(setupPath))
+            {
+                // Run installer running as SYSTEM with admin rights
+                Process.Start(setupPath, "/VERYSILENT")?.WaitForExit(10000 * 60);
+            }
+
+            MarkFileAsDone(setupPath);
+        }
 
 
-		private static void MarkFileAsDone(string setupPath)
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				try
-				{
-					if (File.Exists(setupPath))
-					{
-						File.Delete(setupPath);
-					}
+        private static bool IsSignatureValid(string path)
+        {
+            try
+            {
+                X509Certificate certificate = X509Certificate.CreateFromSignedFile(path);
+                X509Certificate2 fileCertificate = new X509Certificate2(certificate);
+                return ValidCertificateHash == fileCertificate?.Thumbprint;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
 
-					return;
-				}
-				catch (Exception)
-				{
-					// Ignore exception, retry delete file again after a short pause
-					Thread.Sleep(TimeSpan.FromSeconds(1));
-				}
-			}
-		}
-	}
+
+        private static string GetTaskConfigPath(string configName, int startHour, int startMinute)
+        {
+            DateTime now = DateTime.Now;
+            string location = typeof(Program).Assembly.Location;
+
+            string date = now.ToString("s");
+            string startBoundary = new DateTime(now.Year, now.Month, now.Day, startHour, startMinute, 0)
+                .ToString("s");
+            string command = location;
+
+            string templateText = GetTaskConfigTemplate(configName);
+            string text = templateText
+                .Replace("{$Date}", date)
+                .Replace("{$StartBoundary}", startBoundary)
+                .Replace("{$Command}", command);
+
+            string filePath = $"{location}.{configName}.xml";
+            File.WriteAllText(filePath, text);
+
+            return filePath;
+        }
+
+
+        private static string GetTaskConfigTemplate(string configName)
+        {
+            Assembly programAssembly = typeof(Program).Assembly;
+            string name = programAssembly.FullName.Split(',')[0];
+            string resourceName = $"{name}.{configName}.xml";
+
+            using (Stream stream = programAssembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+
+        private static void MarkFileAsDone(string setupPath)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    if (File.Exists(setupPath))
+                    {
+                        File.Delete(setupPath);
+                    }
+
+                    return;
+                }
+                catch (Exception)
+                {
+                    // Ignore exception, retry delete file again after a short pause
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+            }
+        }
+    }
 }
