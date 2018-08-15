04dbee242641350f4106b1132239642084c04ca4
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-08-15 15:20:21 +0200

    Fixz VSE icon

--- Dependinator/ModelViewing/Private/DataHandling/Dtos/DataNodeName.cs
+++ Dependinator/ModelViewing/Private/DataHandling/Dtos/DataNodeName.cs
@@ -1,4 +1,7 @@
-﻿using Dependinator.Utils;
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Dependinator.Utils;
 
 
 namespace Dependinator.ModelViewing.Private.DataHandling.Dtos
@@ -6,7 +9,7 @@ namespace Dependinator.ModelViewing.Private.DataHandling.Dtos
     internal class DataNodeName : Equatable<DataNodeName>
     {
         public static readonly DataNodeName None = new DataNodeName("");
-
+        private static readonly SHA256 Hash = SHA256.Create();
         private readonly string fullName;
 
 
@@ -21,6 +24,26 @@ namespace Dependinator.ModelViewing.Private.DataHandling.Dtos
         public static explicit operator DataNodeName(string fullName) => new DataNodeName(fullName);
         public static explicit operator string(DataNodeName dataName) => dataName.fullName;
 
+
+        public string AsId()
+        {
+            byte[] dataBytes = Encoding.UTF8.GetBytes(fullName);
+            byte[] hashBytes = ComputeHash(dataBytes);
+            string base64String = Convert.ToBase64String(hashBytes, 0, 16);
+
+            return base64String.Substring(0, 22);
+        }
+
         public override string ToString() => fullName;
+
+
+        
+        private static byte[] ComputeHash(byte[] dataBytes)
+        {
+            lock (Hash)
+            {
+                return Hash.ComputeHash(dataBytes);
+            }
+        }
     }
 }
