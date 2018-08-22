fbe45705b880132675956479b31294e624163f3e
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-08-22 08:03:32 +0200

    Move ApiManager into Cpmmon (from Api)

--- Dependinator/Common/ModelMetadataFolders/Private/ExistingInstanceService.cs
+++ Dependinator/Common/ModelMetadataFolders/Private/ExistingInstanceService.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Threading;
-using Dependinator.Api.ApiHandling;
+using Dependinator.Common.ApiHandling;
 using Dependinator.Utils;
 using Dependinator.Utils.Dependencies;
 using Dependinator.Utils.Threading;
