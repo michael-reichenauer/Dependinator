4af5dc2431a24515368cdc57cfd1e5fc7b99e717
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-01-28 10:25:57 +0100

    ok

--- Web/src/Canvas.js
+++ Web/src/Canvas.js
@@ -19,8 +19,6 @@ class Canvas extends Component {
         let rsp = new draw2d.policy.canvas.ReadOnlySelectionPolicy()
         let psp = new draw2d.policy.canvas.PanningSelectionPolicy()
 
-        //let rsp = new PanningPolicy()
-        let selection = null
         rsp.onClick = (figure, mouseX, mouseY, shiftKey, ctrlKey) => {
             console.log('rsp click;', figure, mouseX, mouseY, shiftKey, ctrlKey)
             if (figure !== null) {
@@ -39,42 +37,69 @@ class Canvas extends Component {
             }
         }
 
+        // let f1 = new draw2d.shape.node.Between();
+        // f1.setWidth(100);
+        // f1.setHeight(100);
+        // canvas.add(f1, 100, 100);
 
-        // canvas.add(new draw2d.shape.basic.Oval(), 100, 100);
-        // canvas.add(new draw2d.shape.basic.Rectangle(), 120, 150);
+        // let f2 = new draw2d.shape.node.Between();
+        // f2.setWidth(100);
+        // f2.setHeight(100);
+        // canvas.add(f2, 300, 300);
 
-        let f1 = new draw2d.shape.node.Start();
-        f1.setWidth(100);
-        f1.setHeight(100);
-        canvas.add(f1, 100, 100);
-        // canvas.add(new draw2d.shape.node.Start(), 50, 50);
-
-        let f2 = new draw2d.shape.node.End(100, 100);
-        f2.setWidth(100);
-        f2.setHeight(100);
-        canvas.add(f2, 300, 300);
-        // canvas.add(new draw2d.shape.node.End(), 350, 250);
 
         let nwp = new WheelZoomPolicy()
 
         canvas.installEditPolicy(rsp)
         canvas.installEditPolicy(nwp);
-
+        // canvas.setDimension(new draw2d.geo.Rectangle(0, 0, 50, 50))
 
         canvas.getCommandStack().addEventListener(function (e) {
             if (e.isPostChangeEvent()) {
                 console.log('event:', e)
             }
         });
+
+        let f = new draw2d.shape.node.Between();
+        f.setWidth(100);
+        f.setHeight(100);
+        canvas.add(f, 3000, 3000);
+        canvas.setDimension(new draw2d.geo.Rectangle(0, 0, 10000, 10000))
+
+        console.time('figures')
+        for (var i = 0; i < 10; i++) {
+            setTimeout(() => {
+                for (var i = 0; i < 100; i++) {
+                    let f = new draw2d.shape.node.Between();
+                    f.setWidth(random(10, 50));
+                    f.setHeight(random(10, 50));
+                    f.getPorts().each((i, port) => { port.setVisible(false) })
+                    canvas.add(f, random(0, 8000), random(0, 8000));
+                }
+            }, 0)
+        }
+
+        setTimeout(() => {
+            console.timeEnd('figures')
+        }, 0);
+
     }
 
     render() {
         return (
-            <div style={{ margin: '80px', background: 'red' }}>
-                <div id="canvas" style={{ height: 1000, width: 1000, position: 'absolute', overflow: 'scroll' }}></div>
+            <div style={{ margin: 100, background: 'red' }}>
+                <div id="canvas"
+                    style={{ height: 1000, width: 1000, position: 'absolute', overflow: 'scroll', maxWidth: 1000, maxHeight: 1000 }}></div>
             </div>
         );
     }
 }
+
+function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
 
 export default Canvas;
