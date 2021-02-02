ec9803109cfa99690de7c77b109c532e228fcb21
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-02-02 10:40:33 +0100

    Add app bar

--- Web/src/App.js
+++ Web/src/App.js
@@ -3,16 +3,17 @@ import React from "react";
 import './App.css';
 import Canvas from './Canvas';
 import { useWindowSize } from "./common/windowsize"
-
+import ApplicationBar from './components/ApplicationBar'
 
 function App() {
   const [size] = useWindowSize()
 
   return (
     <div>
-      <div style={{ margin: 100, backgroundColor: "green" }}>
-        <Canvas width={size.width - 200} height={size.height - 200} />
-      </div>
+      <ApplicationBar />
+
+      <Canvas width={size.width} height={size.height - 70} />
+
     </div>
 
   );
---
+++ Web/src/components/ApplicationBar.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Typography, fade, AppBar, Toolbar, IconButton, Tooltip } from "@material-ui/core";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+// import { ApplicationMenu } from "./ApplicationMenu"
+import PublishIcon from '@material-ui/icons/Publish';
+import GetAppIcon from '@material-ui/icons/GetApp';
+
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import RefreshIcon from '@material-ui/icons/Refresh';
+
+
+export default function ApplicationBar({ commands }) {
+    const classes = useAppBarStyles();
+
+    return (
+        <AppBar position="static" style={{ height: "55px" }}>
+            <Toolbar>
+                <Typography className={classes.title} variant="h6" noWrap>Dependinator</Typography>
+
+                <Tooltip title="Refresh list" ><IconButton onClick={() => { }}><RefreshIcon /></IconButton></Tooltip>
+
+
+                {/* <ApplicationMenu /> */}
+
+            </Toolbar>
+        </AppBar >
+    )
+}
+
+
+
+const useAppBarStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '8ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
