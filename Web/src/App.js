08676003004a80f36c272b2f4e62c0375d6d8a53
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-02-03 10:16:07 +0100

    Clean

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import './App.css';
 import Canvas from './Canvas';
 import { useWindowSize } from "./common/windowsize"
 import ApplicationBar from './components/ApplicationBar'
