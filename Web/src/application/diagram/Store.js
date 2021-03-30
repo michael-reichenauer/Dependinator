fca32409dc6ba8869a23c1e4b987c956ea66855c
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-03-30 08:29:53 +0200

    Clean

--- Web/src/application/diagram/Store.js
+++ Web/src/application/diagram/Store.js
@@ -248,7 +248,7 @@ class Store {
         }
 
         return keys.map(key => this.readData(key)).filter(data => data != null)
-            .sort((d1, d2) => d1.canvasId === 'root' ? -1 : d2.canvasId === 'root' ? 1 : 0)
+            .sort((d1, d2) => d1.canvasId === rootCanvasId ? -1 : d2.canvasId === rootCanvasId ? 1 : 0)
     }
 
 
