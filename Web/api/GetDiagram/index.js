44dd104dad9a2d7148869756407b6e4ecf414679
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-04-03 17:24:08 +0200

    Try

--- Web/api/GetDiagram/index.js
+++ Web/api/GetDiagram/index.js
@@ -1,4 +1,4 @@
-var store = require('../shared/Store.js');
+//var store = require('../shared/Store.js');
 //var auth = require('../shared/auth.js');
 
 module.exports = async function (context, req) {
@@ -11,7 +11,8 @@ module.exports = async function (context, req) {
         //  const clientPrincipal = auth.getClientPrincipal(req)
 
         //context.log('parameters', req.body)
-        const diagram = await store.getDiagram(context, diagramId)
+        //const diagram = await store.getDiagram(context, diagramId)
+        const diagram = await getDiagram(context, diagramId)
 
         context.res = { status: 200, body: diagram };
         //context.res = { status: 200, body: 'diagram' };
@@ -19,4 +20,35 @@ module.exports = async function (context, req) {
         context.log.error('error:', err);
         context.res = { status: 400, body: `error: '${err.message}', ${err.stack}` };
     }
+}
+
+const getDiagram = async (context, diagramId) => {
+    const tableName = getTableName(context)
+    //if (clientInfo.token === '12345') {
+    await table.createTableIfNotExists(tableName)
+    // }
+
+
+    let tableQuery = new azure.TableQuery()
+        .where('diagramId == ?string?', diagramId);
+
+    const items = await table.queryEntities(tableName, tableQuery, null)
+
+
+    const diagram = { diagramData: { diagramId: diagramId }, canvases: [] }
+
+    items.forEach(i => {
+        if (i.type === 'diagram') {
+            diagram.diagramData = toDiagramInfo(i)
+        } else if (i.type === 'canvas') {
+            diagram.canvases.push(toCanvasData(i))
+        }
+    })
+
+    return diagram
+}
+
+function getTableName(context) {
+    const info = clientInfo.getInfo(context)
+    return baseTableName + info.token
 }
