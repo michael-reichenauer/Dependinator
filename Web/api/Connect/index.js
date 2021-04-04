e66a5cf71afc9ea37c650b7d205f950f9c0a280a
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-04-04 20:45:13 +0200

    try connect

--- Web/api/Connect/index.js
+++ Web/api/Connect/index.js
@@ -2,14 +2,10 @@ var store = require('../shared/Store.js');
 
 module.exports = async function (context, req) {
     try {
-        const { diagramId } = req.query
-        if (!diagramId) {
-            throw new Error('Invalid args')
-        }
 
-        const diagram = await store.connect(context, diagramId)
+        const connectData = await store.connect(context)
 
-        context.res = { status: 200, body: diagram };
+        context.res = { status: 200, body: connectData };
     } catch (err) {
         context.log.error('error:', err);
         context.res = { status: 400, body: `error: '${err.message}', ${err.stack}` };
