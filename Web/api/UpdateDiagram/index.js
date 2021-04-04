2f6dd4050992bc6c85ccdc687ad93b3c1f4817d6
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-04-04 07:13:57 +0200

    Fix diagram name

--- Web/api/UpdateDiagram/index.js
+++ Web/api/UpdateDiagram/index.js
@@ -2,7 +2,7 @@ var store = require('../shared/Store.js');
 
 module.exports = async function (context, req) {
     try {
-        const diagramInfo = await store.updateDiagram(context, info, req.body)
+        const diagramInfo = await store.updateDiagram(context, req.body)
 
         context.res = { status: 200, body: diagramInfo };
     } catch (err) {
