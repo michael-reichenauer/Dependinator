a6aeab85f42be0228d1ad060b577cd02f68a3387
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-07-29 11:11:03 +0200

    Got encryption to work

--- Web/src/App.js
+++ Web/src/App.js
@@ -7,6 +7,7 @@ import Diagram from "./application/Diagram";
 import About from "./application/About";
 import Login from "./application/Login";
 import AlertDialog from "./common/AlertDialog";
+import { dataCrypt } from './common/dataCrypt';
 
 
 function App() {
@@ -15,6 +16,25 @@ function App() {
   // Enable user activity detection (e.g. moving mouse ) and new available web site at server detection
   useActivityMonitor()
   useAppVersionMonitor()
+
+  const org = '123456'
+  const password = 'abcd'
+
+  const edp = dataCrypt.encryptWithPassword(org, password)
+  edp.then(ed => {
+    console.log('original: ', org)
+    console.log('encrypted:', ed)
+    const ddp = dataCrypt.decryptWithPassword(ed, password)
+    ddp.then(dd => {
+      console.log('decrypted:', dd)
+      if (dd !== org) {
+        console.error('Not same data', dd, org)
+      }
+    })
+      .catch(e => console.error(e))
+  })
+
+
 
   return (
     <>
---
+++ Web/src/common/authenticate.js
@@ -0,0 +1,33 @@
+import { store } from "../application/diagram/Store";
+
+
+class Authenticate {
+    async createUser(user) {
+        // Reduce risk of clear text password logging
+        user.password = await authenticate.passwordHash(user.password)
+
+        await store.createUser(user)
+    }
+
+    async connectUser(user) {
+        user.password = await authenticate.passwordHash(user.password)
+        return await store.connectUser(user)
+    }
+
+    async passwordHash(text) {
+        // encode as UTF-8
+        const msgBuffer = new TextEncoder().encode(text);
+
+        // hash the message
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+        // convert ArrayBuffer to Array
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+        // convert bytes to hex string                  
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        return hashHex;
+    }
+}
+
+export const authenticate = new Authenticate()
