e2ea24ad37041221c05d654a8402dcb010f7593c
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-08-29 10:10:03 +0200

    Clean nodes

--- Web/src/application/Nodes.js
+++ Web/src/application/Nodes.js
@@ -11,6 +11,7 @@ import { FixedSizeList } from 'react-window';
 const subItemsSize = 9
 const iconsSize = 30
 const subItemsHeight = iconsSize + 6
+const allIcons = icons.getAllIcons()
 
 const nodesAtom = atom(false)
 const useStyles = makeStyles((theme) => ({
@@ -35,40 +36,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const useNodes = () => useAtom(nodesAtom)
-const allIcons = icons.getAllIcons()
-
-
-const filterItems = (root, filter) => {
-    const filters = filter.split(' ')
-    const items = allIcons.filter(item => {
-        // Check if root items match (e.g. searching Azure or Aws)
-        if (item.root !== root) {
-            return false
-        }
-
-        // This filter uses implicit 'AND' between words in the search filter
-        // Check if all words are part of the items full name
-        for (var i = 0; i < filters.length; i++) {
-            if (!item.fullName.toLowerCase().includes(filters[i])) {
-                return false
-            }
-        }
-        return true
-    })
-
-    // Some icons exist in multiple groups, lets get unique items
-    const uniqueItems = items.filter(
-        (item, index) => index === items.findIndex(it => it.src === item.src && it.name === item.name))
-
-    return uniqueItems
-}
 
 
 export default function Nodes() {
-    const classes = useStyles();
     const [show, setShow] = useNodes()
-    const [openAzure, setOpenAzure] = useState(false);
-    const [openAws, setOpenAws] = useState(false);
     const [filter, setFilter] = useState('');
 
     useEffect(() => {
@@ -77,18 +48,9 @@ export default function Nodes() {
         return () => PubSub.unsubscribe('nodes.showDialog')
     }, [setShow])
 
-    const onChangeSearch = (value) => {
-        setFilter(value.toLowerCase())
-    }
-
-    const cancelSearch = () => {
-        setFilter('')
-    }
-
-    // Expand Azure/aws sub items
-    const handleAzureClick = () => setOpenAzure(!openAzure)
-    const handleAwsClick = () => setOpenAws(!openAws)
-
+    // Handle search
+    const onChangeSearch = (value) => setFilter(value.toLowerCase())
+    const cancelSearch = () => setFilter('')
 
     const clickedItem = (item) => {
         // show value can be a position or just 'true'. Is position, then the new node will be added
@@ -101,6 +63,40 @@ export default function Nodes() {
         PubSub.publish('canvas.AddNode', { icon: item.key, position: position })
         setShow(false)
     }
+
+
+    return (
+        <Dialog open={show ? true : false} onClose={() => { setShow(false) }}  >
+            <Box style={{ width: 400, height: 530, padding: 20 }}>
+
+                <Typography style={{ paddingBottom: 10 }} >Add Node</Typography>
+
+                <SearchBar
+                    value={filter}
+                    onChange={(searchVal) => onChangeSearch(searchVal)}
+                    onCancelSearch={() => cancelSearch()}
+                />
+
+                <Paper style={{ maxHeight: 440, overflow: 'auto' }}>
+                    <List component="nav" dense disablePadding>
+                        {NodeItemsList('Azure', filter, clickedItem)}
+                        {NodeItemsList('Aws', filter, clickedItem)}
+                    </List>
+                </Paper>
+
+            </Box>
+        </Dialog >
+    )
+}
+
+
+// Items list for Azure and Aws (which can be filtered)
+const NodeItemsList = (root, filter, clickedItem) => {
+    const classes = useStyles();
+    const items = filterItems(root, filter)
+    const height = Math.min(items.length, subItemsSize) * subItemsHeight
+    const [open, setOpen] = useState(false);
+    const iconsSrc = icons.getIcon(root).src
 
     const renderRow = (props, items) => {
         const { index, style } = props;
@@ -116,67 +112,48 @@ export default function Nodes() {
         );
     }
 
-    // Azure and Aws items
-    const azureItems = filterItems('Azure', filter)
-    const azureHeight = Math.min(azureItems.length, subItemsSize) * subItemsHeight
-    const renderAzureRow = (props) => renderRow(props, azureItems)
-    const awsItems = filterItems('Aws', filter)
-    const awsHeight = Math.min(awsItems.length, subItemsSize) * subItemsHeight
-    const renderAwsRow = (props) => renderRow(props, awsItems)
-
-
-
-
-
-
     return (
-        <Dialog open={show ? true : false} onClose={() => { setShow(false) }}  >
-
-            <Box style={{ width: 400, height: 530, padding: 20 }}>
-
-                <Typography style={{ paddingBottom: 10 }} >Add Node</Typography>
-
-                <SearchBar
-                    value={filter}
-                    onChange={(searchVal) => onChangeSearch(searchVal)}
-                    onCancelSearch={() => cancelSearch()}
-                />
-
-                <Paper style={{ maxHeight: 440, overflow: 'auto' }}>
-                    <List component="nav" dense disablePadding>
-
-                        {/* Azure items */}
-                        <ListItem button onClick={handleAzureClick}>
-                            <ListItemIcon>
-                                <img src={icons.getIcon('Azure').src} alt='' width={iconsSize} height={iconsSize} />
-                            </ListItemIcon>
-                            <ListItemText primary="Azure" />
-                            {openAzure ? <ExpandLess /> : <ExpandMore />}
-                        </ListItem>
-                        <Collapse in={openAzure} timeout="auto" unmountOnExit>
-                            <FixedSizeList width={400} height={azureHeight} itemSize={subItemsHeight} itemCount={azureItems.length} >
-                                {renderAzureRow}
-                            </FixedSizeList>
-                        </Collapse>
-
-                        {/* Aws items */}
-                        <ListItem button onClick={handleAwsClick}>
-                            <ListItemIcon>
-                                <img src={icons.getIcon('Aws').src} alt='' width={iconsSize} height={iconsSize} />
-                            </ListItemIcon>
-                            <ListItemText primary="Aws" />
-                            {openAws ? <ExpandLess /> : <ExpandMore />}
-                        </ListItem>
-                        <Collapse in={openAws} timeout="auto" unmountOnExit>
-                            <FixedSizeList width={400} height={awsHeight} itemSize={subItemsHeight} itemCount={awsItems.length} >
-                                {renderAwsRow}
-                            </FixedSizeList>
-                        </Collapse>
-
-                    </List>
-                </Paper>
-            </Box>
-        </Dialog >
+        <>
+            <ListItem button onClick={() => setOpen(!open)}>
+                <ListItemIcon>
+                    <img src={iconsSrc} alt='' width={iconsSize} height={iconsSize} />
+                </ListItemIcon>
+                <ListItemText primary={root} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <FixedSizeList width={385} height={height} itemSize={subItemsHeight} itemCount={items.length} >
+                    {(props) => renderRow(props, items)}
+                </FixedSizeList>
+            </Collapse>
+        </>
     )
 }
 
+// Filter node items based on root and filter
+// This filter uses implicit 'AND' between words in the search filter
+const filterItems = (root, filter) => {
+    const filterWords = filter.split(' ')
+
+    const items = allIcons.filter(item => {
+        // Check if root items match (e.g. searching Azure or Aws)
+        if (item.root !== root) {
+            return false
+        }
+
+        // Check if all filter words are part of the items full name
+        for (var i = 0; i < filterWords.length; i++) {
+            if (!item.fullName.toLowerCase().includes(filterWords[i])) {
+                return false
+            }
+        }
+
+        return true
+    })
+
+    // Some icons exist in multiple groups, lets get unique items
+    const uniqueItems = items.filter(
+        (item, index) => index === items.findIndex(it => it.src === item.src && it.name === item.name))
+
+    return uniqueItems
+}
