2e2f55ff1270a832b200a7db3a6a9309bbcc373d
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-11-14 10:04:35 +0100

    tmp

--- Web/src/common/utils.ts
+++ Web/src/common/utils.ts
@@ -5,31 +5,31 @@ const humanizeDuration = require("humanize-duration");
 
 
 // Returns a duration as a nice human readable string
-export const durationString = duration => {
+export const durationString = (duration:number):string => {
     return humanizeDuration(duration)
 }
 
 // Returns a random number between min and max
-export const random = (min, max) => {
+export const random = (min:number, max:number):number => {
     min = Math.ceil(min);
     max = Math.floor(max) + 1;
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 // Returns the distance between 2 points
-export const distance = (x1, y1, x2, y2) => {
+export const distance = (x1:number, y1:number, x2:number, y2:number):number => {
     return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
 }
 
 // Async sleep/delay
-export async function delay(time) {
+export async function delay(time:number) {
     return new Promise(res => {
         setTimeout(res, time)
     })
 }
 
 // Returns the sha 256 hash of the string
-export async function sha256Hash(text) {
+export async function sha256Hash(text:string) {
     // encode as UTF-8
     const msgBuffer = new TextEncoder().encode(text);
 
