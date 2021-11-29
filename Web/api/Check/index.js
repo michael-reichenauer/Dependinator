b83b2b896c1a58f0ab33e11accd988fe201ab780
Michael Reichenauer <michael.reichenauer@gmail.com>
2021-11-29 08:16:04 +0100

    Clean

--- Web/api/Check/index.js
+++ Web/api/Check/index.js
@@ -2,12 +2,9 @@ var store = require('../shared/Store.js');
 
 module.exports = async function (context, req) {
     try {
-        console.log('api: checking')
         store.verifyApiKey(context)
-        console.log('api: verified')
 
         context.res = { status: 200, body: '' };
-        console.log('api: result')
     } catch (err) {
         context.log.error('error:', err);
         context.res = { status: 400, body: `error: '${err.message}'` };
