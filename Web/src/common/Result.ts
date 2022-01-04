1bb3040b9466b60024738e2272f5bd1e7c316fc4
Michael Reichenauer <michael.reichenauer@gmail.com>
2022-01-04 08:53:51 +0100

    Add isError() support for specific Error types

--- Web/src/common/Result.ts
+++ Web/src/common/Result.ts
@@ -1,12 +1,25 @@
 // The Result type, which can be either the expected value or an Error value. The caller can use
 import assert from "assert";
+
 // Narrowing using either the isError(result) function or the instanceof Error operator
 type Result<T, E = Error> = T | E;
 export default Result;
 
+type ClassType<T> = { new (...args: any[]): T };
+
 // isError(result) returns true if result is an Error and can be used in narrowing by the caller
-export function isError<T, E>(result: Result<T, E>): result is E {
-  return result instanceof Error;
+// isError(result, ErrorType), with specified error type returns true if result is an
+// the specified error type and can be used in narrowing by the caller
+export function isError<T, E>(
+  result: Result<T, E>,
+  errorClass?: ClassType<E>
+): result is E {
+  if (errorClass === undefined) {
+    // No specified error type, assuming Error
+    return result instanceof Error;
+  }
+
+  return result instanceof errorClass;
 }
 
 // orDefault() returns the result or the default value if result is an Error
