e472d7062768a69762fd87a9f8c1790a38c0a8cd
Michael Reichenauer <michael.reichenauer@gmail.com>
2022-02-24 10:54:04 +0100

    Adjust authenication credential handling

--- Web/src/common/authenticate.ts
+++ Web/src/common/authenticate.ts
@@ -13,6 +13,9 @@ export interface IAuthenticate {
   resetLogin(): void;
 }
 
+const minUserName = 2;
+const minPassword = 4;
+
 @singleton(IAuthenticateKey)
 export class Authenticate implements IAuthenticate {
   constructor(
@@ -30,9 +33,11 @@ export class Authenticate implements IAuthenticate {
     return await this.api.check();
   }
 
-  public async createUser(user: User): Promise<Result<void>> {
-    // Expand/derive the password
-    user.password = await this.dataCrypt.expandPassword(user);
+  public async createUser(enteredUser: User): Promise<Result<void>> {
+    const user = await this.hashAndExpandUser(enteredUser);
+    if (isError(user)) {
+      return user;
+    }
 
     const wrappedDek = await this.dataCrypt.generateWrappedDataEncryptionKey(
       user
@@ -41,10 +46,11 @@ export class Authenticate implements IAuthenticate {
     return await this.api.createAccount({ user: user, wDek: wrappedDek });
   }
 
-  public async login(user: User): Promise<Result<void>> {
-    // Expand/derive the password
-    const hash = await this.dataCrypt.expandPassword(user);
-    user.password = hash;
+  public async login(enteredUser: User): Promise<Result<void>> {
+    const user = await this.hashAndExpandUser(enteredUser);
+    if (isError(user)) {
+      return user;
+    }
 
     const loginRsp = await this.api.login(user);
     if (isError(loginRsp)) {
@@ -65,5 +71,50 @@ export class Authenticate implements IAuthenticate {
 
     // Try to logoff from server ass well (but don't await result)
     this.api.logoff();
+  }
+
+  private async hashAndExpandUser(enteredUser: User): Promise<Result<User>> {
+    let { username, password } = enteredUser;
+    console.log(`org '${username}', '${password}'`);
+
+    if (
+      !username ||
+      !password ||
+      username.length < minUserName ||
+      password.length < minPassword
+    ) {
+      return new AuthenticateError();
+    }
+
+    // Normalize username and password
+    username = username.trim().toLowerCase();
+    password = password.trim();
+
+    console.log(`norm '${username}', '${password}'`);
+
+    // Hash username to ensure original username is hidden from server
+    username = await this.toSha256(username);
+
+    // Expand/derive the password to ensure that password is hard to crack using brute force
+    password = await this.dataCrypt.expandPassword({
+      username: username,
+      password: password,
+    });
+
+    console.log(`hash '${username}', '${password}'`);
+
+    return { username: username, password: password };
+  }
+
+  private async toSha256(text: string): Promise<string> {
+    const msgBuffer = new TextEncoder().encode(text);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+    return hashHex;
   }
 }
