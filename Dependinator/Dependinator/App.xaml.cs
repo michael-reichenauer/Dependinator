ce4d274ae03cd66d25a91e5e38951ba8990c246f
Michael Reichenauer <michael.reichenauer@gmail.com>
2023-09-14 06:14:06 +0200

    Initial add of Maui Blazor Hybrid project

---
+++ Dependinator/Dependinator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Dependinator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Dependinator/Dependinator/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Dependinator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
