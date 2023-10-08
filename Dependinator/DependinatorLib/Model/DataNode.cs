c8264969a832dc9a755a4e19f352bdd29cddc772
Michael Reichenauer <michael.reichenauer@gmail.com>
2023-10-08 19:49:00 +0200

    Clean

--- Dependinator/DependinatorLib/Model/DataNode.cs
+++ Dependinator/DependinatorLib/Model/DataNode.cs
@@ -25,11 +25,11 @@ internal class DataNode : Equatable<DataNode>, IDataItem
     public NodeType NodeType { get; }
 
 
-    public string Description { get; set; }
+    public string Description { get; set; } = "";
     public Rect Bounds { get; set; } = new Rect(0, 0, 0, 0);
     public double Scale { get; set; }
-    public string Color { get; set; }
-    public string ShowState { get; set; }
+    public string Color { get; set; } = "";
+    public string ShowState { get; set; } = "";
     public bool IsModified { get; set; }
     public bool HasModifiedChild { get; set; }
     public bool HasParentModifiedChild { get; set; }
