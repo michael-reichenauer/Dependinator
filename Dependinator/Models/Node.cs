644351a39696e45e1e9f492313d1491c12267f7e
Michael Reichenauer <michael.reichenauer@gmail.com>
2024-05-26 06:51:41 +0200

    Try with larger icons

--- Dependinator/Models/Node.cs
+++ Dependinator/Models/Node.cs
@@ -6,7 +6,7 @@ namespace Dependinator.Models;
 
 class Node : IItem
 {
-    const double MinContainerZoom = 2.0;
+    const double MinContainerZoom = 3.0;
     const double MaxNodeZoom = 5 * 1 / Node.DefaultContainerZoom;           // To large to be seen
 
     public Node(string name, Node parent)
