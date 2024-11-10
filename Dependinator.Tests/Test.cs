6dc8dc49fb955e26c7492e92015680c315a666da
Michael Reichenauer <michael.reichenauer@gmail.com>
2024-11-10 06:56:35 +0100

    Add support for mocking

--- Dependinator.Tests/Test.cs
+++ Dependinator.Tests/Test.cs
@@ -1,11 +1,26 @@
 
 namespace Dependinator.Tests;
 
+interface ISome
+{
+    string Get(string name);
+}
+
+class SomeSome(ISome some)
+{
+    public string Get(string name) => some.Get(name);
+}
+
 public class Test
 {
     [Fact]
     public async Task TestAsync()
     {
+        var someMock = new Mock<ISome>(MockBehavior.Strict);
+        someMock.Setup(s => s.Get("name")).Returns("nameX");
+
+        var some = new SomeSome(someMock.Object);
+        Assert.Equal("nameX", some.Get("name"));
 
     }
 }
--- Dependinator.Tests/Usings.cs
+++ Dependinator.Tests/Usings.cs
@@ -2,3 +2,6 @@ global using Xunit;
 global using Dependinator.Utils;
 global using Dependinator.Utils.Logging;
 global using static Dependinator.Utils.Result;
+global using Moq;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DynamicProxyGenAssembly2")]
