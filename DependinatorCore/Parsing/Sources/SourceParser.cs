eae027d568de71e8fe54ca1a610b0cf04a7b0fa8
Michael Reichenauer <michael.reichenauer@gmail.com>
2026-02-17 11:28:52 +0100

    Fix issue building wasm

--- DependinatorCore/Parsing/Sources/SourceParser.cs
+++ DependinatorCore/Parsing/Sources/SourceParser.cs
@@ -17,7 +17,7 @@ interface ISourceParser
 [Transient]
 class SourceParser : ISourceParser
 {
-    public Task<R<IReadOnlyList<Parsing.Item>>> ParseAsync(string slnPath, bool isSkipTests = true)
+    public Task<R<IReadOnlyList<Parsing.Item>>> ParseSolutionAsync(string slnPath, bool isSkipTests = true)
     {
         return Task.FromResult<R<IReadOnlyList<Parsing.Item>>>(
             R.Error($"Source parsing is not supported in browser runtime: {slnPath}.")
