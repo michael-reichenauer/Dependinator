857b2befde0027dff8d6fe451a181753101d38e8
Michael Reichenauer <michael.reichenauer@gmail.com>
2026-03-09 09:55:10 +0100

    Trigger modeld change on toggle hidden node

--- Dependinator/Diagrams/SelectionService.cs
+++ Dependinator/Diagrams/SelectionService.cs
@@ -311,6 +311,7 @@ class SelectionService : ISelectionService
 
         modelService.CheckLineVisibility();
 
+        applicationEvents.TriggerModelChanged();
         applicationEvents.TriggerUIStateChanged();
     }
 
