d5d3cb773d4a348aa6c08c9d4cd8daaf65ef6fe6
Michael Reichenauer <michael.reichenauer@gmail.com>
2026-03-27 15:20:53 +0100

    Exclude view data from model sync hash calc

--- Dependinator.Tests/Shared/CloudModelSerializerTests.cs
+++ Dependinator.Tests/Shared/CloudModelSerializerTests.cs
@@ -35,4 +35,50 @@ public class CloudModelSerializerTests
         Assert.Equal(modelDto.Offset, roundTrippedModel.Offset);
         Assert.Equal(modelDto.ViewRect, roundTrippedModel.ViewRect);
     }
+
+    [Fact]
+    public void GetContentHash_ShouldIgnoreViewStateFields()
+    {
+        ModelDto dto1 = new()
+        {
+            Name = "test",
+            Zoom = 1.5,
+            Offset = new Pos(10, 20),
+            ViewRect = new Rect(1, 2, 3, 4),
+            Nodes = [],
+            Links = [],
+        };
+        ModelDto dto2 = new()
+        {
+            Name = "test",
+            Zoom = 3.0,
+            Offset = new Pos(99, 99),
+            ViewRect = new Rect(5, 6, 7, 8),
+            Nodes = [],
+            Links = [],
+        };
+
+        Assert.Equal(
+            CloudModelSerializer.GetContentHash(dto1),
+            CloudModelSerializer.GetContentHash(dto2)
+        );
+    }
+
+    [Fact]
+    public void CreateDocument_HashShouldMatchGetContentHash()
+    {
+        ModelDto modelDto = new()
+        {
+            Name = "test",
+            Zoom = 2.0,
+            Offset = new Pos(5, 5),
+            ViewRect = new Rect(1, 1, 10, 10),
+            Nodes = [],
+            Links = [],
+        };
+
+        CloudModelDocument document = CloudModelSerializer.CreateDocument("test", modelDto);
+
+        Assert.Equal(CloudModelSerializer.GetContentHash(modelDto), document.ContentHash);
+    }
 }
